485486017b646aa81c8f0c57e1c7dd751257a52a
Bilalsk <bilalshaikh624@gmail.com>
2019-01-07 17:33:53 +0530

    Setup Api for Heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 
+const port = process.env.PORT || 3000;
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -112,6 +114,6 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log('Started on port '+port);
 });
